0e2cd48c68fdd1fbac525d46fe5aa79c344e04cd
Moh7ven <mohamedsangare0107@gmail.com>
2023-01-07 15:48:26 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
 import "./App.css";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [nom, setNom] = useState({ nom: "Konate", prenom: "Bakary" });
-  const [x, setX] = useState(0);
-  const changeNom = () => {
-  setNom({nom:"Koffi",prenom:"Anon"})
-}
+  const [dataRecu, setRecu] = useState({});
+  const [nom, setNom] = useState("Mohamed");
+
+  useEffect(() => {
+    setNom("sedigui");
+  }, []);
+  const getData = useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency",
+      params: { have: "USD", want: "EUR", amount: "5000" },
+      headers: {
+        "X-RapidAPI-Key": "4cbcfa3368msh8efec9911878d3bp1e7a20jsn6f6eac938593",
+        "X-RapidAPI-Host": "currency-converter-by-api-ninjas.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setRecu(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <>
-      <div>{nom.nom}</div>
-      <div>{nom.prenom}</div>
-      <div>
-        <button onClick={changeNom}>clic pour changer de nom</button>
-      </div>
-      <div>{x}</div>
-      <div>
-        <button
-          onClick={() => {
-            setX((prev) => prev + 1);
-          }}
-        >
-          clic
-        </button>
-      </div>
-    </>
+    <div>
+      <div>{nom}</div>
+      <h3>
+        <li>Ancienne devise: {dataRecu.old_currency}</li>
+      </h3>
+      <h3>
+        <li>Ancien Montant: {dataRecu.old_amount}</li>
+      </h3>
+      <h3>
+        <li>Nouvelle devise : {dataRecu.new_currency}</li>
+      </h3>
+      <h3>
+        <li>Nouveau montant : {dataRecu.new_amount}</li>
+      </h3>
+    </div>
   );
 }
 
